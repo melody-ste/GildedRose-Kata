73a9c5dabfe5e0b7f15c0b778d372ab4dd30b94b
melody-ste <melody_stephan@icloud.com>
2025-09-23 18:31:25 +0200

    move the test from texttest_fixture to gilded_rose_spec ( jasmine folder )

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,36 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
+const {Shop, Item} = require('../src/gilded_rose.js');
+
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  it("full test", () => {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
+
+      // This Conjured item does not work properly yet
+      new Item("Conjured Mana Cake", 3, 6),
+    ];
+
+    const days = Number(process.argv[2]) || 2;;
+    const gildedRose = new Shop(items);
+
+    for (let day = 0; day < days; day++) {
+      console.log(`\n-------- day ${day} --------`);
+      console.log("name, sellIn, quality");
+      items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+      gildedRose.updateQuality();
+    }
   });
 
+  it("should foo", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("fixme");
+  });
 });
